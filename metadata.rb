b01fd7dd558eec62a2ed76777fd3d65c2e0fbc0c
tristanfisher <tristan@amplify.com>
2013-03-22 14:01:13 -0400

    recipe name is default - not cookbookname

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures the Charybdis IRC server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-recipe "charybdis", "Configures a basic IRC server"
+recipe "default", "Configures a basic IRC server"
 
 %w{ debian ubuntu }.each do |os|
   supports os
